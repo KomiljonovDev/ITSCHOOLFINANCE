54e0c981762338083574fb995da2711783bde466
komiljonovdev <uzokdeveloper@gmail.com>
2022-09-27 11:32:02 -0700

    remove income direction and addworker and remove worker methods added successfully

--- API/v1/index.php
+++ API/v1/index.php
@@ -50,11 +50,12 @@
                 	$db->insertInto('income_direction',[
                 		'name'=>$name,
                 		'des'=>$description,
+                		'timestamp'=>strtotime('now')
                 	]);
                 	$incomedirection = $db->selectWhere('income_direction',[
                 		[
                 			'id'=>0,
-                			'cn'=>'=>'
+                			'cn'=>'>='
                 		]
                 	]);
                 	$data['ok'] = true;
@@ -63,12 +64,98 @@
             		foreach ($incomedirection as $key => $value) $data['result'][$key] = $value;
                 }else{
                 	$data['code'] = 403;
-                	$data['message'] = 'login or password is invalid';
+                	$data['message'] = 'token is invalid';
                 }
 			}else{
 				$data['code'] = 402;
                 $data['message'] = 'token and income direction name is required';
 			}
+		}else if ($action == 'removeincomedirection') {
+			if (isset($token) && isset($id)) {
+                if (isManager($token)) {
+                	$db->delete('income_direction',[
+                		[
+                			'id'=>$id,
+                			'cn'=>"="
+                		],
+                	]);
+                	$incomedirection = $db->selectWhere('income_direction',[
+                		[
+                			'id'=>0,
+                			'cn'=>'>='
+                		]
+                	]);
+                	$data['ok'] = true;
+            		$data['code'] = 200;
+            		$data['message'] = 'income direction deleted successfully';
+            		foreach ($incomedirection as $key => $value) $data['result'][$key] = $value;
+                }else{
+                	$data['code'] = 403;
+                	$data['message'] = 'token is invalid';
+                }
+			}else{
+				$data['code'] = 402;
+                $data['message'] = 'token and income direction name is required';
+			}
+		}else if($action == 'addworker'){
+			if (isset($token) && isset($name) && isset($lastname)) {
+                if (isManager($token)) {
+                	$description = ($description) ? $description : '';
+                	$percent = ($percent) ? $percent : 50.00;
+                	$db->insertInto('workers',[
+                		'name'=>$name,
+                		'lastname'=>$lastname,
+                		'des'=>$description,
+                		'percent'=>$percent,
+                		'timestamp'=>strtotime('now')
+                	]);
+                	$incomedirection = $db->selectWhere('workers',[
+                		[
+                			'id'=>0,
+                			'cn'=>'>='
+                		]
+                	]);
+                	$data['ok'] = true;
+            		$data['code'] = 200;
+            		$data['message'] = 'worker added successfully';
+            		foreach ($incomedirection as $key => $value) $data['result'][$key] = $value;
+                }else{
+                	$data['code'] = 403;
+                	$data['message'] = 'token is invalid';
+                }
+			}else{
+				$data['code'] = 402;
+                $data['message'] = 'token and worker name or worker lastname name is required';
+			}
+		}else if($action == 'removeworker'){
+			if (isset($token) && isset($id)) {
+                if (isManager($token)) {
+                	$db->delete('workers',[
+                		[
+                			'id'=>$id,
+                			'cn'=>"="
+                		],
+                	]);
+                	$incomedirection = $db->selectWhere('workers',[
+                		[
+                			'id'=>0,
+                			'cn'=>'>='
+                		]
+                	]);
+                	$data['ok'] = true;
+            		$data['code'] = 200;
+            		$data['message'] = 'workers deleted successfully';
+            		foreach ($incomedirection as $key => $value) $data['result'][$key] = $value;
+                }else{
+                	$data['code'] = 403;
+                	$data['message'] = 'token is invalid';
+                }
+			}else{
+				$data['code'] = 402;
+                $data['message'] = 'token and income direction name is required';
+			}
+		}else if(false){
+
 		}else{
 			$data['code'] = 401;
             $data['message'] = 'Method not found';
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ITSCHOOLFINANCE
-ITSCHOOL financial website
+# IT SCHOOL financial website
+IT school molayaviy holatini hisoblab turuvchi,ochiqlikni yo'lga qo'yuvchi va ishni osonlashtiruvchi web-sayti uchun CRM API
